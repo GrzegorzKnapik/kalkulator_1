bcb748482bbfc3af0ea3777c95472b577887dfa0
GrzesiekKnapik <grzesiek2001@interia.pl>
2022-10-01 01:15:24 +0200

    #

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
